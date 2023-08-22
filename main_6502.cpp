04370599e18472d519d9ba2d1977ff0fe1e45fd0
rianb <rianborah@gmail.com>
2023-08-22 15:39:51 +0530

    Init commit

---
+++ main_6502.cpp
@@ -0,0 +1,15 @@
+/*
+ *  @author:    Rian Borah
+ *  @date:      22 Aug, 2023
+ *
+ *  @desc:      An emulator for the 6502 microprocessor
+ *
+ *  @ref:       http://www.6502.org/users/obelisk/
+ * */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}

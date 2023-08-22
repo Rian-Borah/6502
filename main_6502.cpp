2413dd890243cc57bdd4bad6dd4927d11ae52185
rianb <rianborah@gmail.com>
2023-08-22 18:09:05 +0530

    Done with 2 instructions - LDA_IM & LDA_ZP

--- main_6502.cpp
+++ main_6502.cpp
@@ -8,22 +8,25 @@
  * */
 
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
 #include <cstdlib>
-#include <string.h>
+#include <cstring>
+#include <cinttypes>
+#include <cerrno>
 
-using Byte = unsigned char;
-using Word = unsigned short;
+using byte = uint8_t;
+using word = uint16_t;
 
 /*
  *  struct Mem
  *  @date:      22 Aug, 2023
  *  @desc:      Memory blocks for the 6502 processor
+ *  @note:      Processor is little endian
+ *              Addresses stored in memory using LSB
  * */
 struct Mem{
     static constexpr uint32_t MAX_MEM = 1024 * 64;
-    Byte Data[MAX_MEM];
+    byte data[MAX_MEM];
 
 public:
     /*
@@ -34,8 +37,40 @@ public:
      *  @return:    None
      * */
     void init(){
-        memset(Data, 0, sizeof(Data));
+        memset(data, 0, sizeof(data));
         return;
+    }
+
+    /*
+     *  operator[]
+     *
+     *  @desc:      Operator overload to read 1 byte from memory block
+     *  @param:     addr - Address to read from
+     *  @return:    1 byte from memory block
+     * */
+    byte operator[](uint32_t addr) const{
+        if(addr >= MAX_MEM){
+            fprintf(stderr, "ERROR: Invalid read address %d", addr);
+            perror("");
+            exit(EXIT_FAILURE);
+        }
+        return data[addr];
+    }
+
+    /*
+     *  operator[]
+     *
+     *  @desc:      Operator overload to write 1 byte to memory block
+     *  @param:     addr - Address to write to
+     *  @return:    1 byte from memory block
+     * */
+    byte& operator[](uint32_t addr){
+        if(addr >= MAX_MEM){
+            fprintf(stderr, "ERROR: Invalid write address %d", addr);
+            perror("");
+            exit(EXIT_FAILURE);
+        }
+        return data[addr];
     }
 };
 
@@ -46,19 +81,19 @@ public:
  *  @desc:      CPU internal registers for the 6502 processor
  * */
 struct CPU{
-    Word PC;        // program counter - 16bit
-    Byte SP;        // stack pointer - 8bit
+    word PC;        // program counter - 16bit
+    byte SP;        // stack pointer - 8bit
 
-    Byte A, X, Y;   // registers
+    byte A, X, Y;   // registers
 
     // processor status bitflags : using C++ bitfields
-    Byte C : 1;     // carry
-    Byte Z : 1;     // zero
-    Byte I : 1;     // interrupt disable
-    Byte D : 1;     // decimal
-    Byte B : 1;     // break
-    Byte V : 1;     // overflow
-    Byte N : 1;     // negative
+    byte C : 1;     // carry
+    byte Z : 1;     // zero
+    byte I : 1;     // interrupt disable
+    byte D : 1;     // decimal
+    byte B : 1;     // break
+    byte V : 1;     // overflow
+    byte N : 1;     // negative
 
 public:
     /*
@@ -71,10 +106,89 @@ public:
      * */
     void reset(Mem& memory){
         PC = 0xFFFC;
-        SP = 0xFD;  // decremented 3 times from 0xFF for three fake push operations
+        SP = 0xFF;  // decremented 3 times from 0xFF for three fake push operations
         C = Z = I = D = B = V = N = 0;
         A = X = Y = 0x00;
         memory.init();
+        return;
+    }
+
+    /*
+     *  fetch()
+     *
+     *  @desc:      Fetches a single instruction from memory
+     *  @param:     cycles - Number of cycles to fetch based on instruction
+     *              memory - 6502 memory
+     *  @return:    Instruction byte
+     * */
+    byte fetch(uint32_t& cycles, Mem& memory){
+        byte data = memory[PC];
+        PC++;
+        cycles--;
+        return data;
+    }
+
+    /*
+     *  read()
+     *
+     *  @desc:      Read a single instruction from memory
+     *  @param:     cycles - Number of cycles to fetch based on instruction
+     *              memory - 6502 memory
+     *  @return:    Instruction byte
+     * */
+    byte read(uint32_t& cycles, uint32_t addr, Mem& memory){
+        byte data = memory[addr];
+        cycles--;
+        return data;
+    }
+
+    /*
+     *  LDASetStatus()
+     *
+     *  @desc:      Set status flags for LDA instructions
+     * */
+    void LDASetStatus(){
+        Z = (A == 0);               // set if A = 0
+        N = (A & (0b1 << 7)) == 1;  // set if bit 7 in A is set
+        return;
+    }
+
+    // opcodes
+    static constexpr byte
+        INS_LDA_IM = 0xA9,
+        INS_LDA_ZP = 0xA5;
+
+
+    /*
+     *  execute()
+     *
+     *  @desc:      Executes a single instruction
+     *  @param:     cycles - Number of cycles to fetch based on instruction
+     *              memory - 6502 memory
+     *  @return:    None
+     * */
+    void execute(uint32_t cycles, Mem& memory){
+        while(cycles > 0){
+            byte inst = fetch(cycles, memory);
+            switch (inst) {
+
+            case INS_LDA_IM:{
+                byte val = fetch(cycles, memory);
+                A = val;
+                LDASetStatus();
+            } break;
+
+            case INS_LDA_ZP:{
+                byte zpaddr = fetch(cycles, memory);
+                A = read(cycles, zpaddr, memory);
+                LDASetStatus();
+            } break;
+
+            default:{
+                printf("ERROR: Instruction not recognized: %d", inst);
+            } break;
+            }
+        }
         return;
     }
 };
@@ -90,10 +204,19 @@ public:
  *  @return:    EXIT_SUCCESS or EXIT_FAILURE, based on runtime
  * */
 int main() {
-    // processor is little endian
-    // addresses stored in memory using LSB
     Mem mem6502;
     CPU cpu6502;
 
-    return EXIT_SUCCESS;
+    cpu6502.reset(mem6502);
+
+    // hardcoded inline program to test
+    // start
+    mem6502[0xFFFC] = CPU::INS_LDA_ZP;
+    mem6502[0xFFFD] = 0x42;
+    mem6502[0x42] = 0x84;
+    // end
+
+    cpu6502.execute(3, mem6502);
+
+    exit(EXIT_SUCCESS);
 }
